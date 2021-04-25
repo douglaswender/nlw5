8b9cd967266832962da6538937f24527b1f33056
Douglas Wender <dglswender@gmail.com>
2021-04-25 17:59:29 -0400

    Upando codigo

---
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -0,0 +1,69 @@
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:dev_quiz/core/app_images.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
+import 'package:flutter/material.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  final String title;
+  final String completed;
+  final double percent;
+  final VoidCallback onTap;
+
+  const QuizCardWidget({
+    Key? key,
+    required this.title,
+    required this.completed,
+    required this.percent,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.fromBorderSide(
+              BorderSide(color: AppColors.border),
+            ),
+            color: AppColors.white),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Image.asset(AppImages.blocks),
+              height: 40,
+              width: 40,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              title,
+              style: AppTextStyles.heading15,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    completed,
+                    style: AppTextStyles.body11,
+                  ),
+                ),
+                Expanded(
+                    flex: 4, child: ProgressIndicatorWidget(value: percent))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
